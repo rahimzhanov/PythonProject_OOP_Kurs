994deb60212c715188d1be3d8fa5e8507623a65c
Rakhimzhanov Aman <amanrahim2@gmail.com>
2025-11-08 00:16:39 +0500

    get_top_vacancies in utils ready

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,21 @@
-def sort_vacancies(vacancies):
+from typing import List
+
+
+def sort_vacancies(vacancies: List['Vacancy']) -> List['Vacancy']:
+    """
+    Сортирует список вакансий по убыванию зарплаты.
+
+    Вакансии сортируются от самой высокой к самой низкой зарплате
+    на основе поля salary_from класса Vacancy.
+    """
     return sorted(vacancies, reverse=True)
+
+def get_top_vacancies(n: int, vacancies: List['Vacancy']) -> List['Vacancy']:
+    """
+    Возвращает список топ N вакансий
+    :param n:
+    :param vacancies:
+    :return: list
+    """
+    sorted_list = sort_vacancies(vacancies)
+    return sorted_list[:n]
